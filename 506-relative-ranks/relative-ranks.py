d7f559872e9a61ae987e74e17531024d6ff4f595
toedue <83957140+toedue@users.noreply.github.com>
2025-06-27 19:04:56 +0300

    Time: 0 ms (100.00%) | Memory: 19 MB (59.30%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        idexed_score = [(x,y) for y,x in enumerate(score)]
+        output = [""] * len(score)
+        sorted_idexed_score = sorted(idexed_score, key = lambda x: x[0], reverse = True)
+
+        for i , (x,y) in enumerate(sorted_idexed_score):   #[(10, 0), (9, 3), (8, 2), (4, 4), (3, 1)]
+            if i == 0:
+                output[y] = "Gold Medal"
+            elif i == 1:
+                output[y] = "Silver Medal"
+            elif i == 2:
+                output[y] = "Bronze Medal"
+            else:
+                output[y] = str(i+1)
+
+        return output
+
+
+
