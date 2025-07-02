ee07a1f6f1b5dba355b6ebda6bd5db3c3caeec20
toedue <83957140+toedue@users.noreply.github.com>
2025-07-02 17:48:49 +0300

    Time: 6 ms (21.38%) | Memory: 17.6 MB (99.81%) - LeetSync

---
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        result = []
+        result2= []
+
+        for i in arr2:
+            for j in arr1:
+                if j == i:
+                    result.append(j)
+
+
+        for i in arr1:
+            if i not in arr2:
+                result2.append(i)
+        result2.sort()
+        result.extend(result2)
+
+        return result
+
