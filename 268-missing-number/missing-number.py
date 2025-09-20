261aa327c31240eb69b3f69ad91fdc4656473064
toedue <83957140+toedue@users.noreply.github.com>
2025-09-21 00:57:57 +0300

    Time: 3 ms (60.39%) | Memory: 18.7 MB (51.19%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        for i in range(len(nums)):
-            if i != nums[i]:
-                return i
-        return len(nums)
+        # nums.sort()
+        # for i in range(len(nums)):
+        #     if i != nums[i]:
+        #         return i
+        # return len(nums)
+
+        # sum(iterable, start=0) takes two arguments:
+        # iterable → a sequence of numbers (like a list, tuple, or range)
+        # start → a number to start adding from (default is 0)
+
+        return sum(range(len(nums)+1)) - sum(nums)
