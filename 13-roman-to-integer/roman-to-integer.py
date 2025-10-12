8687486bcea5dfc4425db4cd885443cd2ca8b246
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-10-12 17:11:33 +0300

    Time: 7 ms (41.66%) | Memory: 18 MB (20.62%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -2,10 +2,10 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         dict_ = {"I":1,"V":5,"X":10, "L":50, "C":100,"D":500,"M":1000}
         result = 0
-        for i in range(len(s) - 1):
+        for i in range(len(s) - 1): 
             if dict_[s[i]] < dict_[s[i+1]]:
                 result -= dict_[s[i]]
-            else: 
+            else: #dict_[s[i]] < dict_[s[i+1]] and #dict_[s[i]] = dict_[s[i+1]]
                 result += dict_[s[i]]
 
         result += dict_[s[-1]] # add the last one
