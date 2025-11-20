4c866fe76f9febfda01595e9dbac2fb8607dfc55
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-11-20 23:45:45 +0300

    Time: 0 ms (100.00%) | Memory: 17.5 MB (96.91%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        x = bin(x)[2:].zfill(32)
+        y = bin(y)[2:].zfill(32)
+        ans = 0
+        for i in range(32):
+            if str(x)[i] != str(y)[i]:
+                ans += 1
+        return ans
