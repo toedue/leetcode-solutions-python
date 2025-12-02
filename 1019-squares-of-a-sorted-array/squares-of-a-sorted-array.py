58aeefaa344b98caeb4a65cc1dc2f23ca03e2f92
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-12-02 11:09:00 +0300

    Time: 15 ms (24.78%) | Memory: 19.9 MB (11.20%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -2,35 +2,19 @@ class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         pointer1 = 0
         pointer2 = len(nums) - 1
-
-        while pointer1 <= pointer2 :
-            nums[pointer1] = nums[pointer1]*nums[pointer1]
-             
-            if pointer1 != pointer2:  # avoid squaring same middle element twice
-                nums[pointer2] = nums[pointer2]*nums[pointer2]
-
-            pointer1 += 1
-            pointer2 -= 1
-
-        # nums.sort()    
-        # to beat the O(nlogn)
-        l = 0
-        r = len(nums)-1
         result = []
 
-        while (l<=r):
-            if nums[l] >= nums[r]:
-                result.append(nums[l])
-                l+=1
-            else:
-                result.append(nums[r])
-                r -= 1
+        while pointer1 <= pointer2 :
+            if abs(nums[pointer1]) < abs(nums[pointer2]):
+                result.append(nums[pointer2]**2)
+                pointer2 -= 1
+            else: # abs(nums[pointer1]) >= abs(nums[pointer2]):
+                result.append(nums[pointer1]**2)
+                pointer1+=1
+
         return result[::-1]
             
 
-
-
-        return nums
 
 
       
