c4e3eededed93ffbbc77d3aa64204d0f010345fe
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-12-04 21:33:33 +0300

    Time: 1776 ms (19.20%) | Memory: 18.5 MB (79.15%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,19 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pointer_1 = 0
-        pointer_2 = len(nums) -1
-        # [3,2,4]
-        nums_with_indices = [(value,index) for index, value in enumerate(nums)]
-        # [(3,0),(2,1),(4,2)]
-
-        nums_with_indices.sort()
-
-        # [(2,1),(3,0),(4,2)]
-
-        while pointer_1 < pointer_2:
-            if nums_with_indices[pointer_1][0] + nums_with_indices[pointer_2][0] == target:
-                return [nums_with_indices[pointer_1][1],nums_with_indices[pointer_2][1]]
-            elif nums_with_indices[pointer_1][0] + nums_with_indices[pointer_2][0] > target:
-                pointer_2 -= 1
-            elif nums_with_indices[pointer_1][0] + nums_with_indices[pointer_1][0] < target:
-                pointer_1 += 1
+        for i in range(len(nums)-1):
+            for j in range(i + 1, len(nums) ):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
