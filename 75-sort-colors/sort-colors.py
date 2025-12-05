8b8fde4e0cd7488bf91bfacf3e911a86b5815f62
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-12-05 23:23:25 +0300

    Time: 3 ms (12.29%) | Memory: 18.2 MB (11.28%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,27 +1,20 @@
-def merge(left,right):
-    i,j = 0,0
-    result = []
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i+=1
-        else:
-            result.append(right[j])
-            j+=1
-    result.extend(left[i:])
-    result.extend(right[j:])
-    return result
-
-
-def merge_sort(arr):
+def quick_sort(arr):
     if len(arr) <= 1:
         return arr
-    
-    mid = len(arr)//2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
+    pivot = arr[-1]
+    left = []
+    right = []
 
-    return merge(left,right)
+    for x in arr[:-1]:  #execpt the pivot
+        if x <= pivot:
+            left.append(x)
+        else:
+            right.append(x)
+
+    sorted_left = quick_sort(left)
+    sorted_right = quick_sort(right)
+
+    return sorted_left + [pivot] + sorted_right
 
 
 
@@ -30,91 +23,94 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # If list is empty or has one element, it's already sorted
-        sorted_nums = merge_sort(nums)
+
+        sorted_arr = quick_sort(nums)
+
         for i in range(len(nums)):
-            nums[i] = sorted_nums[i]
+            nums[i] = sorted_arr[i]
 
 
 
+    """
+
+    arr = [2,0,2,1,1,0]
+    pivot = 0
 
 
-        """
-        Merge Sort
-        Merge sort is a divide-and-conquer algorithm:
-        split the array into halves until size ≤ 1 (base case),
-
-        merge sorted halves back together.
-        Time: O(n log n). Space: O(n) (for merging).
-
-        Step 1 — Divide
-        Split the array into two halves until each half becomes only 1 element.
-        example:
-        [2, 0, 2, 1]  
-        → split → [2, 0] and [2, 1]  
-        → split → [2], [0], [2], [1]
-
-        Step 2 — Conquer (Sort each half)
-        Each small array with 1 element is already sorted.
-
-        Step 3 — Merge
-        Merge two sorted halves into one big sorted array:
-
-        Merge [2] and [0] → [0, 2]
-        Merge [2] and [1] → [1, 2]
-
-        Final merge:
-        [0, 2] + [1, 2] → [0, 1, 2, 2]
-        
+    | x | compare to pivot=0 | goes to |
+    | - | ------------------ | ------- |
+    | 2 | 2 > 0              | right   |
+    | 0 | 0 <= 0             | left    |
+    | 2 | 2 > 0              | right   |
+    | 1 | 1 > 0              | right   |
+    | 1 | 1 > 0              | right   |
 
 
-        example2:
-        nums = [2, 0, 2, 1, 1, 0]
+    left  = [0]
+    right = [2,2,1,1]
 
-        Step 1 — Split:
+    Recursively sort left
+    quick_sort([0])
+    Length 1 → return [0]
+    
+    Recursively sort right:
+    quick_sort([2,2,1,1])
 
-        Left half:  [2, 0, 2]
-        Right half: [1, 1, 0]
+    pivot = 1
+    | x | <=1? | goes to |
+    | - | ---- | ------- |
+    | 2 | no   | right   |
+    | 2 | no   | right   |
+    | 1 | yes  | left    |
 
-        Split again:
-        Left:
-        [2]  [0,2] → [0] [2]
+    left  = [1]
+    right = [2,2]
 
-        Right:
-        [1]  [1,0] → [1] [0]
+    sort left:
+    quick_sort([1]) → returns [1]
+    
+    sort right:
+    quick_sort([2,2])
+    left = [2]
+    right = []
+    pivot = 2
 
-        Step 2 — Merge small pieces:
-
-        [0] + [2] = [0,2]
-        [1] + [0] = [0,1]
-
-        Left side becomes:
-        [2] + [0,2] = [0,2,2]
-        Right side becomes:
-        [1] + [0,1] = [0,1,1]
-
-        Step 3 — Final Merge:
-        [0,2,2] + [0,1,1] = [0,0,1,1,2,2]
-
+    sort left:
+    quick_sort([2]) → returns [2]
 
 
-                                      [2,0,2,1,1,0]
-                                /                         \
-                         [2,0,2]                           [1,1,0]
-                      /          \                      /          \
-                   [2]         [0,2]                [1]          [1,0]
-                                /    \                            /    \
-                             [0]    [2]                       [1]      [0]
+    sorted result = [2] + [2] + []
+               = [2,2]
 
-                             → merge → [0,2]                → merge → [0,1]
+    Combine Step:
 
-                      → merge → [0,2,2]                → merge → [0,1,1]
+    sorted_left = [1]
+    sorted_right = [2,2]
+    pivot = 1
 
-                       FINAL MERGE → [0,0,1,1,2,2]
+    → [1] + [1] + [2,2] = [1,1,2,2]
+    
+    Final Combine:
+
+    left_sorted = [0]
+    pivot = 0
+    right_sorted = [1,1,2,2]
+    
+    [0] + [0] + [1,1,2,2] = [0,0,1,1,2,2]
 
 
 
 
 
 
-        """
+
+
+
+
+
+
+
+
+    """
+
+      
