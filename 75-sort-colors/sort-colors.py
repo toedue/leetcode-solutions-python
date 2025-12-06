17e99bfca9dab10cbf871a615c7ceb62f58aaaec
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-12-06 22:32:12 +0300

    Time: 4 ms (5.78%) | Memory: 17.7 MB (53.15%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,21 +1,3 @@
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[-1]
-    left = []
-    right = []
-
-    for x in arr[:-1]:  #execpt the pivot
-        if x <= pivot:
-            left.append(x)
-        else:
-            right.append(x)
-
-    sorted_left = quick_sort(left)
-    sorted_right = quick_sort(right)
-
-    return sorted_left + [pivot] + sorted_right
-
 
 
 class Solution:
@@ -23,94 +5,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        def counting_sort(arr):
+            maxx = max(arr)
 
-        sorted_arr = quick_sort(nums)
+            # Step 1: count frequencies
+            counts = [0] * (maxx + 1)
+            for x in arr:
+                counts[x] += 1
 
-        for i in range(len(nums)):
-            nums[i] = sorted_arr[i]
+            # Step 2: fill back the array
+            i = 0
+            for c in range(maxx + 1):
+                while counts[c] > 0:
+                    arr[i] = c
+                    i += 1
+                    counts[c] -= 1
 
-
-
-    """
-
-    arr = [2,0,2,1,1,0]
-    pivot = 0
-
-
-    | x | compare to pivot=0 | goes to |
-    | - | ------------------ | ------- |
-    | 2 | 2 > 0              | right   |
-    | 0 | 0 <= 0             | left    |
-    | 2 | 2 > 0              | right   |
-    | 1 | 1 > 0              | right   |
-    | 1 | 1 > 0              | right   |
-
-
-    left  = [0]
-    right = [2,2,1,1]
-
-    Recursively sort left
-    quick_sort([0])
-    Length 1 → return [0]
-    
-    Recursively sort right:
-    quick_sort([2,2,1,1])
-
-    pivot = 1
-    | x | <=1? | goes to |
-    | - | ---- | ------- |
-    | 2 | no   | right   |
-    | 2 | no   | right   |
-    | 1 | yes  | left    |
-
-    left  = [1]
-    right = [2,2]
-
-    sort left:
-    quick_sort([1]) → returns [1]
-    
-    sort right:
-    quick_sort([2,2])
-    left = [2]
-    right = []
-    pivot = 2
-
-    sort left:
-    quick_sort([2]) → returns [2]
-
-
-    sorted result = [2] + [2] + []
-               = [2,2]
-
-    Combine Step:
-
-    sorted_left = [1]
-    sorted_right = [2,2]
-    pivot = 1
-
-    → [1] + [1] + [2,2] = [1,1,2,2]
-    
-    Final Combine:
-
-    left_sorted = [0]
-    pivot = 0
-    right_sorted = [1,1,2,2]
-    
-    [0] + [0] + [1,1,2,2] = [0,0,1,1,2,2]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    """
-
-      
+        counting_sort(nums)
