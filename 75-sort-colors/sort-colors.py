2013f8495ef6413ae08a55359971430979c5abdc
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-12-07 22:01:01 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.11%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,3 +1,18 @@
+def counting_sort(arr):
+    maxx = max(arr) #O(n)
+    counts = [0]* (maxx+1)
+
+    for num in arr:  #O(n)
+        counts[num] +=1
+    
+    i=0
+    for count in range(maxx + 1):
+        while counts[count] > 0:
+            arr[i] = count
+            i +=1
+            counts[count] -= 1
+
+    
 
 
 class Solution:
@@ -5,20 +20,63 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def counting_sort(arr):
-            maxx = max(arr)
-
-            # Step 1: count frequencies
-            counts = [0] * (maxx + 1)
-            for x in arr:
-                counts[x] += 1
-
-            # Step 2: fill back the array
-            i = 0
-            for c in range(maxx + 1):
-                while counts[c] > 0:
-                    arr[i] = c
-                    i += 1
-                    counts[c] -= 1
-
         counting_sort(nums)
+
+
+
+
+
+
+        """
+        arr = [2,0,2,1,1,0]
+
+        1. Count frequencies
+
+        counts = [0,0,0]   # because max value is 2
+
+        | x | counts  |
+        | - | ------- |
+        | 2 | [0,0,1] |
+        | 0 | [1,0,1] |
+        | 2 | [1,0,2] |
+        | 1 | [1,1,2] |
+        | 1 | [1,2,2] |
+        | 0 | [2,2,2] |
+
+        0 appears 2 times  
+        1 appears 2 times  
+        2 appears 2 times  
+
+        2. Write back the sorted array
+
+        i = 0
+        counts = [2,2,2]
+
+Loop through c = 0:
+        counts[0] = 2
+        → arr[0] = 0
+        → arr[1] = 0
+        counts becomes [0,2,2]
+Now c = 1:
+        counts[1] = 2
+        → arr[2] = 1
+        → arr[3] = 1
+        counts becomes [0,0,2]
+Now c = 2:
+        counts[2] = 2
+        → arr[4] = 2
+        → arr[5] = 2
+        counts becomes [0,0,0]
+Final arr:
+        [0,0,1,1,2,2]
+
+
+
+
+
+
+
+
+
+
+        """
