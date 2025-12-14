4c397e3aef45cd1fb8aa1a1b49749f55f4c93839
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-12-14 23:05:43 +0300

    Time: 4 ms (59.82%) | Memory: 19 MB (9.59%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,8 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
-        ptr1 = 0  
+        ptr1 = 0
+        
         for ptr2 in range(len(nums)):
             if nums[ptr2] != 0:
                 nums[ptr1], nums[ptr2] = nums[ptr2], nums[ptr1]
