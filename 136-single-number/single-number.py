84f98be431fed2de76d01b421e0a3cb33a5d97b2
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-12-26 23:34:04 +0300

    Time: 0 ms (100.00%) | Memory: 19 MB (98.67%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,28 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        x =0
+        result = 0
         for num in nums:
-            x^= num
-        return x
+            result ^= num
+        return result
+        
+"""
+XOR = AB'+ A'B
+A B ^
+0 0 0
+0 1 1
+1 0 1
+1 1 0
+
+Duplicate numbers cancel each other
+The remaining number is the answer
+
+nums = [4, 1, 2, 1, 2]
+
+result = 0
+0 ^ 4 = 4  000-100 = 100
+4 ^ 1 = 5  100-001 = 101
+5 ^ 2 = 7  101-010 = 111
+7 ^ 1 = 6  111-001 = 110
+6 ^ 2 = 4  110-010 = 100
+
+"""
