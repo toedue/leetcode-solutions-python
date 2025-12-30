f59a938e2c46844021161df0653a56e2e0b12767
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2025-12-30 20:23:39 +0300

    Time: 7 ms (18.39%) | Memory: 17.4 MB (91.25%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,5 +1,5 @@
 class Solution:
-    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]: #
         result = []
         result2= []
 
