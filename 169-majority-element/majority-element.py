d77a427737ef1b3efbb9e794b0fbc3a2d31233ec
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-01-30 10:10:01 +0300

    Time: 15 ms (17.72%) | Memory: 21.3 MB (9.51%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = defaultdict(int)
+        dic = defaultdict(int) # space complexity O(n)
         n = len(nums)
 
-        #loop on the nums and store it on the dictionary
-        for i in range(n):
+        #loop on the nums and store it on the hashmap with their frequency
+        for i in range(n):  # Time Complexity O(n)
             dic[nums[i]] += 1
 
-        #loop on the dictionary and find the major element
-        for num in nums:
+        #loop on the hashmap and find the major element
+        for num in nums: # Time Complexity O(n)
             if dic[num] > n/2:
                 return (num)
 
