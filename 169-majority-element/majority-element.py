511a3926bcfba2f160d12d95cfd3f85aa53ed935
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-01-30 09:58:31 +0300

    Time: 13 ms (21.33%) | Memory: 21.3 MB (9.51%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,13 @@
 class Solution:
-    #
     def majorityElement(self, nums: List[int]) -> int:
         dic = defaultdict(int)
         n = len(nums)
+
+        #loop on the nums and store it on the dictionary
         for i in range(n):
             dic[nums[i]] += 1
+
+        #loop on the dictionary and find the major element
         for num in nums:
             if dic[num] > n/2:
                 return (num)
