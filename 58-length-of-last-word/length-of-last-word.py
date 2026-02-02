986b545dc7779a26c12d1ea55f73098de4b2b0b2
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-03 02:23:52 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (44.83%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -2,3 +2,10 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         x = s.split()
         return (len(x[-1]))
+
+
+        """The Python split() method is a built-in function that splits a string into a list of substrings based on a specified separator. By default, it uses any sequence of whitespace (spaces, tabs, newlines) as the delimiter and removes empty strings from the result. 
+        
+        symtax:
+        string.split(separator, maxsplit)
+"""
