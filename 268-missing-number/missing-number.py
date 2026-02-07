d9231d427d0ade72a3d8427a1cd43ba7e42c4f8d
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-07 23:53:11 +0300

    Time: 0 ms (100.00%) | Memory: 20.6 MB (25.14%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,13 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # nums.sort()
-        # for i in range(len(nums)):
-        #     if i != nums[i]:
-        #         return i
-        # return len(nums)
+        n = len(nums)
+        sum_of_range = (n * (n+1))//2
 
-        # sum(iterable, start=0) takes two arguments:
-        # iterable → a sequence of numbers (like a list, tuple, or range)
-        # start → a number to start adding from (default is 0)
-
-        return sum(range(len(nums)+1)) - sum(nums)
+        return sum_of_range - sum(nums)
+        
