6258381c96ad897e9183684e132e713f62c14fa6
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-12 15:23:46 +0300

    Time: 23 ms (37.10%) | Memory: 26.2 MB (67.23%) - LeetSync

--- 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
+++ 609-find-duplicate-file-in-system/find-duplicate-file-in-system.py
@@ -1,14 +1,19 @@
 class Solution:
     def findDuplicate(self, paths: List[str]) -> List[List[str]]:
         d = {}
+
+        # looping over the list of strings and split the string by spaces 
         for string in paths:
             parts = string.split()
-            folder = parts[0] 
+            folder = parts[0] # the root path is the first element
 
+            # we loop on the rest of the parts list so to separate the file name and the content inside it
             for i in range(1, len(parts)):
                 file_name, content = parts[i].split("(")
                 content = content[:-1]
-                
+
+
+                # then aim to create a hashmap to store the content as a key and the full path as a value in a list 
                 full_path = folder + "/" + file_name
 
                 if content not in d:
