1fd2a439bd292ab718adb24168f45de4f47847eb
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-19 14:07:24 +0300

    Time: 476 ms (43.08%) | Memory: 143.7 MB (78.91%) - LeetSync

--- 3707-find-the-most-common-response/find-the-most-common-response.py
+++ 3707-find-the-most-common-response/find-the-most-common-response.py
@@ -2,13 +2,9 @@ from collections import Counter
 
 class Solution:
     def findCommonResponse(self, responses: List[List[str]]) -> str:
-        for i in range(len(responses)):
-            temp_set = set(responses[i])
-            responses[i] = list(temp_set)
+        count = Counter()
 
-        flat_list = [response for row in responses for response in row]
-        d = Counter(flat_list)
+        for row in responses:
+            count.update(set(row))
 
-        sorted_items = sorted(d.items(), key= lambda x: (-x[1], x[0]))
-
-        return sorted_items[0][0]
+        return min(count.keys(), key= lambda x: (-count[x],x))
