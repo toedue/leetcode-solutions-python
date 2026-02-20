b28d85f969f82609148d5616a47dd66d7fe57961
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-20 15:43:10 +0300

    Time: 1093 ms (5.06%) | Memory: 19.5 MB (78.43%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -4,9 +4,12 @@ class Solution:
         size = len(heights)
         
         for i in range(size):
+            swaped = False
             for j in range(size - 1, 0, -1):
                 if heights[j] > heights[j-1]:
                     heights[j], heights[j-1] = heights[j-1], heights[j]
                     names[j], names[j-1] = names[j-1], names[j]
+                    swaped = True
+            if swaped == False: break
 
         return names
