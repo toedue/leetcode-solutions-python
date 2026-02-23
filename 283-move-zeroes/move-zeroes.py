bfebe19cd169db22173c15ef84d50cff99a8fb19
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-23 18:33:31 +0300

    Time: 10 ms (20.64%) | Memory: 20.4 MB (73.48%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,8 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         ptr1 = 0
+        ptr2 = 0 
         
-        for ptr2 in range(len(nums)):
+        while(ptr2 < len(nums)):
             if nums[ptr2] != 0:
                 nums[ptr1], nums[ptr2] = nums[ptr2], nums[ptr1]
-                ptr1 += 1
+                ptr1+=1
+            ptr2 +=1
