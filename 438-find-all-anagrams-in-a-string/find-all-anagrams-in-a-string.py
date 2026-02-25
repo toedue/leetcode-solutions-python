1ae25305710e7547e0c5aaa6327b3b6121f6285b
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-25 19:51:22 +0300

    Time: 186 ms (26.51%) | Memory: 20 MB (15.27%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -5,13 +5,31 @@ class Solution:
         target_length = len(p)
         target = Counter(p)
         result = []
+        left = 0
 
-        for i in range(len(s)):
-            current = s[i: i + target_length]
-            current_count = Counter(current)
+        if len(s) < target_length:
+            return result
 
+        current = s[:target_length]
+        current_count = Counter(current)
+
+        if target == current_count:
+                result.append(left)
+
+        for right in range(target_length, len(s)):
+            # remove left character
+            current_count[s[left]] -= 1
+
+            if current_count[s[left]] == 0:
+                del current_count[s[left]]
+
+            # add new right character
+            current_count[s[right]] += 1
+
+            left += 1
+            
             if target == current_count:
-                result.append(i)
+                result.append(left)
 
 
         return result
