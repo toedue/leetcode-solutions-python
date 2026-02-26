3386fe67077f149d4b1dd7888fa79883933ea8c9
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-26 12:01:39 +0300

    Time: 12 ms (8.01%) | Memory: 19.5 MB (24.51%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -3,7 +3,9 @@ class Solution:
         size = len(nums)
         for i, num in enumerate(nums):
             nums[i] = str(num)
+        
 
+        #bubble sort
         for i in range (size):
             for j in range(0, size - 1 - i):
                 if nums[j] + nums[j+1] < nums[j+1] + nums[j]:
