3075353fd4ff933c0f17b40966995e1e6b168324
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-03-04 11:27:30 +0300

    Time: 12 ms (29.79%) | Memory: 22.9 MB (28.81%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -1,19 +1,33 @@
 class NumArray:
 
-    def __init__(self, nums: List[int]):
-        self.arr = []
-        current_sum = 0
-        for i in range(len(nums)):
-            current_sum += nums[i]
-            self.arr.append(current_sum)
-        
+    def __init__ (self, nums: List[int]): # -> int:
+        self.prefix = [0] + nums
+
+        for i in range(1, len(self.prefix)):
+            self.prefix[i] += self.prefix[i - 1]
+
+        # print(self.prefix)
+        # return 0
 
     def sumRange(self, left: int, right: int) -> int:
+         
+        return (self.prefix[right + 1] - self.prefix[left])
+         
 
-        right_sum = self.arr[right]
-        left_sum = self.arr[left -1] if left > 0 else 0
+    # def __init__(self, nums: List[int]):
+    #     self.arr = []
+    #     current_sum = 0
+    #     for i in range(len(nums)):
+    #         current_sum += nums[i]
+    #         self.arr.append(current_sum)
+        
 
-        return right_sum - left_sum
+    # def sumRange(self, left: int, right: int) -> int:
+
+    #     right_sum = self.arr[right]
+    #     left_sum = self.arr[left -1] if left > 0 else 0
+
+    #     return right_sum - left_sum
         
 
 
