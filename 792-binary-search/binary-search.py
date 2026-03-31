36ce1d68ef7943cd5849e889f19de65d2e9d50c9
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-03-31 17:43:59 +0300

    Time: 0 ms (100.00%) | Memory: 20.5 MB (86.16%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -8,7 +8,7 @@ class Solution:
             if target == nums[mid]:
                 return mid
             elif target < nums[mid]:
-                right = mid-1
+                right = mid - 1
             else:
                 left = mid + 1
         return -1
