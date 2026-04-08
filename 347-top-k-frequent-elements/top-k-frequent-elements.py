87dde40b5f311bcb3f31872e6d7f4efb37bc2d29
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-04-08 18:22:51 +0300

    Time: 10 ms (25.56%) | Memory: 25 MB (8.15%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,10 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        store = Counter(nums)
-        sorted_store = sorted(store.items(),key = lambda x:x[1], reverse= True)
-        # output = []
-        # for i in range(k):
-        #     output.append(sorted_store[i][0])
-        # return output
+        # [1,1,1,2,2,3], k = 4
+        # [[], [3], [2], [1, 6, 7]]
+        #  0   1    2   3
+        #  answer = [1, 6, 7, 2]   
 
-        return [key for key, val in sorted_store[:k]]
+        # freq, values, buckets
+        freq = Counter(nums)
+
+        buckets = [[] for _ in range(len(nums)+1)]
+        print(freq)
+        for key, value in freq.items():
+            buckets[value].append(key) # value = frequency, key = value
+
+        answer = []
+        for i in range(len(buckets) - 1, -1, -1):
+            for element in buckets[i]:
+                answer.append(element)
+                if len(answer) == k:
+                    return answer
