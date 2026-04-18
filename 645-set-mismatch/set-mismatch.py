bc5f73fdb58006f119cb20862be97918073e3fba
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-04-19 00:27:48 +0300

    Time: 23 ms (30.07%) | Memory: 20.3 MB (96.80%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,15 +1,16 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        n_sum = (n*(n+1))//2
-        s_sum =0
-        s = set()
-        for num in nums:
-            if num in s:
-                temp = num
-            else:
-                s.add(num)
-        for num in s:
-            s_sum += num
-        missing = n_sum - s_sum
-        return [temp, missing]
+        nums.sort()
+
+        for i in range(1, n):
+            if nums[i-1] == nums[i]:
+                duplicate = nums[i]
+            if nums[i] > nums[i-1] + 1:
+                missing = nums[i-1] + 1
+        if nums[0] != 1:
+            missing = 1
+        elif nums[-1] != n:
+            missing = n
+
+        return [duplicate, missing]
