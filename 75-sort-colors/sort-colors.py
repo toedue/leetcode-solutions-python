3a8aad77c94a9957b051557c765fdf1ad43d5ee2
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-04-19 19:50:27 +0300

    Time: 1 ms (14.13%) | Memory: 19.4 MB (7.68%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,82 +1,18 @@
-def counting_sort(arr):
-    maxx = max(arr) #O(n)
-    counts = [0]* (maxx+1)
-
-    for num in arr:  #O(n)
-        counts[num] +=1
-    
-    i=0
-    for count in range(maxx + 1):
-        while counts[count] > 0:
-            arr[i] = count
-            i +=1
-            counts[count] -= 1
-
-    
-
-
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        counting_sort(nums)
 
+        buckets = [0,0,0]
 
+        for i in range(len(nums)):
+            buckets[nums[i]] += 1
 
-
-
-
-        """
-        arr = [2,0,2,1,1,0]
-
-        1. Count frequencies
-
-        counts = [0,0,0]   # because max value is 2
-
-        | x | counts  |
-        | - | ------- |
-        | 2 | [0,0,1] |
-        | 0 | [1,0,1] |
-        | 2 | [1,0,2] |
-        | 1 | [1,1,2] |
-        | 1 | [1,2,2] |
-        | 0 | [2,2,2] |
-
-        0 appears 2 times  
-        1 appears 2 times  
-        2 appears 2 times  
-
-        2. Write back the sorted array
+        print(buckets)
 
         i = 0
-        counts = [2,2,2]
-
-Loop through c = 0:
-        counts[0] = 2
-        → arr[0] = 0
-        → arr[1] = 0
-        counts becomes [0,2,2]
-Now c = 1:
-        counts[1] = 2
-        → arr[2] = 1
-        → arr[3] = 1
-        counts becomes [0,0,2]
-Now c = 2:
-        counts[2] = 2
-        → arr[4] = 2
-        → arr[5] = 2
-        counts becomes [0,0,0]
-Final arr:
-        [0,0,1,1,2,2]
-
-
-
-
-
-
-
-
-
-
-        """
+        for color in range(3):
+            for _ in range(buckets[color]):
+                nums[i] = color
+                i += 1
