a8525c257e6e18a469c0e9857831b18de1d0bc5e
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-04-22 15:08:39 +0300

    Time: 1 ms (61.86%) | Memory: 19 MB (99.84%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,22 +1,18 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        # create the circle
-        circle = list(range(1, n + 1))  # [1, 2, 3, ... n]
+        circle = [i for i in range(1,n+1)]
 
-        index = 0  # start at position 0 (friend 1)
+        print(circle)
 
+        index = 0
         while len(circle) > 1:
 
-            # move k-1 steps forward from current position
-            # % len(circle) handles wrapping around!
-            index = (index + k - 1) % len(circle)
+            index = (index + (k-1)) % len(circle)
 
-            # remove this friend
             circle.pop(index)
 
             # index now automatically points to next friend
-            # BUT if we removed the last element, wrap to 0
             if index == len(circle):
                 index = 0
 
-        return circle[0]  # last one standing!
+        return circle[index]
